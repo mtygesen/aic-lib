9e071971a075f79c0ed929d0d89fbd98418015b9
icecoldgold773 <mikkel.tygesen@gmail.com>
2023-02-12 02:08:08 +0100

    Changed architecture of CMakeLists

---
+++ searching/build/CMakeFiles/search_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/search_example.dir/search_example.cpp.o"
+  "CMakeFiles/search_example.dir/search_example.cpp.o.d"
+  "search_example"
+  "search_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/search_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ searching/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/icecoldgold773/programming-exercise/algorithms-in-cpp/searching
+# Build directory: /home/icecoldgold773/programming-exercise/algorithms-in-cpp/searching/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

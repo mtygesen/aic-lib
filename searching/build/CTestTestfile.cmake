978003fd4787908cdf80df3cc79f73d2c8b20dd1
icecoldgold773 <mikkel.tygesen@gmail.com>
2023-02-12 01:47:17 +0100

    Removed build and .vscode folder from cache

--- searching/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/icecoldgold773/programming-exercise/algorithms-in-cpp/searching
-# Build directory: /home/icecoldgold773/programming-exercise/algorithms-in-cpp/searching/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

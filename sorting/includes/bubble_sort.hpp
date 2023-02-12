f418df553f60354085148787e96a632e0dbf08c1
icecoldgold773 <mikkel.tygesen@gmail.com>
2023-02-12 14:33:19 +0100

    Added bubblesort

--- sorting/includes/bubble_sort.hpp
+++ sorting/includes/bubble_sort.hpp
@@ -5,7 +5,21 @@
 #include "swap.hpp"
 
 template <class T> void BubbleSort(std::vector<T>& v) {
-    
+    bool flag;
+
+    for (int i = 1; i < v.size() - i; ++i) {
+
+        flag = false;
+
+        for (int j = 0; j <= (n - i - 1); ++j) {
+            if (v[j] > v[j + 1]) {
+                Swap(&v[j], &v[j + 1]);
+                flag = true;
+            }
+        }
+        
+        if (!flag) break;
+    }
 }
 
 #endif

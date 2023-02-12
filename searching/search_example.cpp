e54e69050073d73edf622b442299d44c38ecd61a
Mikkel Tygesen <83021047+icecoldgold773@users.noreply.github.com>
2023-02-12 01:53:32 +0100

    Update search_example.cpp

--- searching/search_example.cpp
+++ searching/search_example.cpp
@@ -4,8 +4,8 @@
 int main() {
 	std::vector<int> vec = {1, 53, 129, 42, 13, 82, 46};
 
-	std::cout << linearSearch(vec, 129) << '\n';
-	std::cout << binarySearch(vec, 129) << '\n';
+	std::cout << linearSearch(vec, 129) << '\n'; // => 2
+	std::cout << binarySearch(vec, 129) << '\n'; // => 2
 
 	return 0;
 }

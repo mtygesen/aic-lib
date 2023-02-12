300d623d8230921ec20d74f69424e7745da92d15
Mikkel Tygesen <83021047+icecoldgold773@users.noreply.github.com>
2023-02-12 02:49:13 +0100

    Update sentinel_search.hpp

--- searching/includes/sentinel_search.hpp
+++ searching/includes/sentinel_search.hpp
@@ -11,7 +11,7 @@
  * @return Index of element or -1 if no element is found
  */
 template <typename T> T sentinelSearch(const std::vector<T>& v, T key) {
-	v.push_back(key);
+    v.push_back(key);
     
     for (auto i = 0; i != target; ++i);
 

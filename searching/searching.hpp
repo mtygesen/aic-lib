4b98c3ac7daefe4c66f5d74964d0853aeb772e71
icecoldgold773 <mikkel.tygesen@gmail.com>
2023-03-02 16:47:54 +0100

    Fixed searching.hpp

--- searching/searching.hpp
+++ searching/searching.hpp
@@ -1,8 +1,8 @@
 #ifndef aic_searching
 #define aic_searching
 
-#include "includes/binary_search.hpp"
-#include "includes/linear_search.hpp"
-#include "includes/sentinel_search.hpp"
+#include "src/binary_search.hpp"
+#include "src/linear_search.hpp"
+#include "src/sentinel_search.hpp"
 
 #endif
